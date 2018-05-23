6568956b195f5ecb2368d9b61c4cf8adc149c575
Duncan Seif <duncan.seif@hotmail.com>
2018-05-23 15:38:39 -0500

    Got all the basic sass in place. Next step is responsive

---
+++ includes/medSpecFaculty.php
@@ -0,0 +1,33 @@
+<div class="medSpecProfiles">
+
+<?php
+
+    $count = 4;
+
+    for ($i=0; $i < $count ; $i++) { 
+       
+?>
+
+    <div class="medSpecProfile">
+        <div class="specImg">
+            <img src="img/animals.jpeg" />
+        </div>
+        <div class="specContent">
+            <h4>Duncan Seif</h4>
+            <p>Web Developer<br />Medical Director, Autopsy</p>
+            <ul>
+                <li>Phone: XXX-XXX-XXXX</li>
+                <li>Email: duncan.seif@northwestern.edu</li>
+            </ul>  
+        </div>
+          
+    </div>
+
+
+<?php 
+
+    }
+?>
+
+
+</div>
