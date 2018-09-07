f0966370b65722ad216e07bf63512c0169607d18
Duncan Seif <duncan.seif@northwestern.edu>
2018-09-07 16:16:06 -0500

    its been way to long. No idea what changes I made and didnt commit

--- index.php
+++ index.php
@@ -1,24 +1,17 @@
 <html>
 <link rel="stylesheet" type="text/css" href="dist/css/flex-icons.min.css">
-<link rel="stylesheet" type="text/css" href="dist/css/embedVid.css">
+<link rel="stylesheet" type="text/css" href="dist/css/cehs-spots.css">
 <script
   src="https://code.jquery.com/jquery-3.2.1.min.js"
   integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
   crossorigin="anonymous"></script>
  
 <body>
-<h1>This is a test</h1>
-
-<section class="hero">
-    <div class="hero-image" href="https://placeholder.com">
-        <img src="http://via.placeholder.com/1440x400">
-            <a class="button" href="https://www.youtube.com/embed/4OZJi1nOiHk?rel=0&enablejsapi=1&autoplay=1">Click Me!</a>
-    </div>
-</section>
+<h1>CEHS Spotlights</h1>
 
 <?php 
 
-//include('includes/flex-icons.php');
+include('includes/cehs-spots.php');
 
 // include('includes/news-index.php');
 
