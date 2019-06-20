16303b8698a862221bfdfda62881605a0635d7f0
Duncan Seif <duncan.seif@northwestern.edu>
2019-06-20 11:01:38 -0500

    Added title to svg hover

--- dist/js/output.js
+++ dist/js/output.js
@@ -14,40 +14,50 @@ function makeSVGdynamic() {
   var a = document.getElementById("prevMedJewel"); // Get the SVG document inside the Object tag
 
   var svgDoc = a.contentDocument;
-  console.log(svgDoc); // Get one of the SVG items by ID;
-
-  var svgItem = svgDoc.getElementById("Public_Health"); // Set the colour to something else
+  console.log(svgDoc); // // Get one of the SVG items by ID;
+  // var svgItem = svgDoc.getElementById("Public_Health");
+  // // Set the colour to something else
 
   var baseURL = 'https://www.preventivemedicine.northwestern.edu';
   var allowedClicks = [{
     'selector': 'Behavioral_Medicine',
     'icon_selector': '#Behavioral_Medicine_Icon',
-    'url': baseURL + '/divisions/behavioral-medicine/index.html'
+    'url': baseURL + '/divisions/behavioral-medicine/index.html',
+    'title': 'Behavior Medicine Division'
   }, {
     'selector': 'Epidemiology',
     'icon_selector': '#Epidemiology_Icon',
-    'url': baseURL + '/divisions/epidemiology/index.html'
+    'url': baseURL + '/divisions/epidemiology/index.html',
+    'title': 'Epidemiology Division'
   }, {
     'selector': 'Nutrition',
     'icon_selector': '#Nutrition_Icon',
-    'url': baseURL + '/divisions/nutrition/index.html'
+    'url': baseURL + '/divisions/nutrition/index.html',
+    'title': 'Nutrition Division'
   }, {
     'selector': 'Biostatistics',
     'icon_selector': '#Biostatistics_Icon',
-    'url': baseURL + '/divisions/biostatistics/index.html'
+    'url': baseURL + '/divisions/biostatistics/index.html',
+    'title': 'Biostatistics Division'
   }, {
     'selector': 'Cancer_Epidemiology_and_Prevention',
     'icon_selector': '#Cancer_Epidemiology_and_Prevention_Icon',
-    'url': baseURL + '/divisions/cancer-epidemiology-and-prevention/index.html'
+    'url': baseURL + '/divisions/cancer-epidemiology-and-prevention/index.html',
+    'title': 'Cancer Epidemiology and Prevention Division'
   }, {
     'selector': 'Health_and_Biomedical_Informatics',
     'icon_selector': '#Health_and_Biomedical_Informatics_Icon',
-    'url': baseURL + '/divisions/health-and-biomedical-informatics/index.html'
+    'url': baseURL + '/divisions/health-and-biomedical-informatics/index.html',
+    'title': 'Health and Biomedical Informatics Division'
   }, {
     'selector': 'Public_Health',
     'icon_selector': '#Public_Health_Icon',
-    'url': baseURL + '/divisions/public-health-practice/index.html'
-  }];
+    'url': baseURL + '/divisions/public-health-practice/index.html',
+    'title': 'Public Health Division'
+  }]; //get a reference to SVG title
+
+  var titleElem = svgDoc.getElementById("Layer_1").getElementsByTagName("title")[0];
+  titleElem.innerHTML = 'Preventive Medicine Jewel';
   allowedClicks.forEach(function (row) {
     try {
       //get a reference to jewel prong path
@@ -66,9 +76,11 @@ function makeSVGdynamic() {
 
       elem.addEventListener("mouseenter", function (e) {
         icon_1.style.opacity = 1;
+        titleElem.innerHTML = row.title;
       });
       elem.addEventListener("mouseleave", function (e) {
         icon_1.style.opacity = .5;
+        titleElem.innerHTML = '';
       });
     } catch (_a) {//do nothing 
     }
--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -21,9 +21,9 @@ function makeSVGdynamic() {
     
     console.log( svgDoc ); 
 
-    // Get one of the SVG items by ID;
-	var svgItem = svgDoc.getElementById("Public_Health");
-    // Set the colour to something else
+    // // Get one of the SVG items by ID;
+	// var svgItem = svgDoc.getElementById("Public_Health");
+    // // Set the colour to something else
 
     const baseURL = 'https://www.preventivemedicine.northwestern.edu';
 
@@ -31,39 +31,50 @@ function makeSVGdynamic() {
             { 
                 'selector'      : 'Behavioral_Medicine',
                 'icon_selector' : '#Behavioral_Medicine_Icon',
-                'url'           : baseURL + '/divisions/behavioral-medicine/index.html'    
+                'url'           : baseURL + '/divisions/behavioral-medicine/index.html',
+                'title'         : 'Behavior Medicine Division'     
             }, 
             {
                 'selector'      : 'Epidemiology',
                 'icon_selector' : '#Epidemiology_Icon',
-                'url'           : baseURL + '/divisions/epidemiology/index.html'
+                'url'           : baseURL + '/divisions/epidemiology/index.html',
+                'title'         : 'Epidemiology Division' 
             },
             {
                 'selector'      : 'Nutrition',
                 'icon_selector' : '#Nutrition_Icon',
-                'url'           : baseURL + '/divisions/nutrition/index.html'
+                'url'           : baseURL + '/divisions/nutrition/index.html',
+                'title'         : 'Nutrition Division' 
             },
             {
                 'selector'      : 'Biostatistics',
                 'icon_selector' : '#Biostatistics_Icon', 
-                'url'           : baseURL + '/divisions/biostatistics/index.html'
+                'url'           : baseURL + '/divisions/biostatistics/index.html',
+                'title'         : 'Biostatistics Division'
             },
             {
                 'selector'      : 'Cancer_Epidemiology_and_Prevention',
                 'icon_selector' : '#Cancer_Epidemiology_and_Prevention_Icon',
-                'url'           : baseURL + '/divisions/cancer-epidemiology-and-prevention/index.html'
+                'url'           : baseURL + '/divisions/cancer-epidemiology-and-prevention/index.html',
+                'title'         : 'Cancer Epidemiology and Prevention Division'
             },
             {
                 'selector'      : 'Health_and_Biomedical_Informatics',
                 'icon_selector' : '#Health_and_Biomedical_Informatics_Icon',
-                'url'           : baseURL + '/divisions/health-and-biomedical-informatics/index.html'
+                'url'           : baseURL + '/divisions/health-and-biomedical-informatics/index.html',
+                'title'         : 'Health and Biomedical Informatics Division'
             },
             {
                 'selector'      : 'Public_Health',
                 'icon_selector' : '#Public_Health_Icon',
-                'url'           : baseURL + '/divisions/public-health-practice/index.html'
+                'url'           : baseURL + '/divisions/public-health-practice/index.html',
+                'title'         : 'Public Health Division'
             }
     ];
+
+    //get a reference to SVG title
+    let titleElem = svgDoc.getElementById("Layer_1").getElementsByTagName("title")[0];
+    titleElem.innerHTML = 'Preventive Medicine Jewel';
 
     allowedClicks.forEach( row => {
         try {
@@ -84,9 +95,11 @@ function makeSVGdynamic() {
             //change icon opacity one mouse enter and leave
             elem.addEventListener("mouseenter", e => {
                 icon.style.opacity = 1;
+                titleElem.innerHTML = row.title;
             });
             elem.addEventListener("mouseleave", e => {
                 icon.style.opacity = .5;
+                titleElem.innerHTML = '';
             });
         } catch {
             //do nothing 
