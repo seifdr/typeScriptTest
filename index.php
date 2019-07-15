153a253129bd8ec4d57b0da2c0d1dbecfb8b9dfb
Duncan Seif <duncan.seif@northwestern.edu>
2019-07-15 12:18:50 -0500

    Got the pathology sliders fixed so image does going behind text anymore on safari

--- index.php
+++ index.php
@@ -1,20 +1,66 @@
-<html>
-<!-- <link rel="stylesheet" type="text/css" href="dist/css/flex-icons.min.css"> -->
-<link rel="stylesheet" type="text/css" href="dist/css/prevmed-graphics.css">
-<script
-  src="https://code.jquery.com/jquery-3.2.1.min.js"
-  integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
-  crossorigin="anonymous"></script>
- 
-<body>
-<?php 
+<!doctype html>
+      <!--[if lt IE 7]> <html class="ie6 oldie"> <![endif]-->
+      <!--[if IE 7]>    <html class="ie7 oldie"> <![endif]-->
+      <!--[if IE 8]>    <html class="ie8 oldie"> <![endif]-->
+      <!--[if gt IE 8]><!--> <html class="" lang="en"><!--<![endif]-->
+      <head>
+          <meta content="text/html; charset=utf-8" http-equiv="Content-Type"/>
+          <meta content="width=device-width, initial-scale=1, minimum-scale=1" name="viewport"/>
+          <meta content="IE=Edge" http-equiv="X-UA-Compatible"/>
+          
+          <meta content="The Department of Psychiatry and Behavioral Sciences at Northwestern University Feinberg School of Medicine is committed to forward-thinking research and exceptional education for the next generation of leaders in our field." name="description"/>
+          <meta content="97a2540a8169772433cf2a058d5dc6dd" name="pageID"/>
+          <meta content="Home" property="og:title"/>
+          <meta content="The Department of Psychiatry and Behavioral Sciences at Northwestern University Feinberg School of Medicine is committed to forward-thinking research and exceptional education for the next generation of leaders in our field." property="og:description"/>
+          <meta content="//www.psychiatry.northwestern.edu/images/1440-400/psychiatry-resident-education-1440-400.jpg" property="og:image"/>
+          
+          <title>Home: Department of Psychiatry &amp; Behavioral Sciences: Feinberg School of Medicine: Northwestern University</title>
 
-  include('includes/prevmed-graphic.php'); 
+          <!-- BEGIN CSS_COMMON --><link href="//deptcommon.fsm.northwestern.edu/_fw/styles/NM-dept-responsive.css" rel="stylesheet" type="text/css"/><link href="//maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css" rel="stylesheet" type="text/css"/><!-- END CSS_COMMON -->
+          <!-- BEGIN CSS_EXTRA --><link href="//deptcommon.fsm.northwestern.edu/_fw/styles/nu-fw-landing-page-style.css" rel="stylesheet" type="text/css"/><link href="//deptcommon.fsm.northwestern.edu/_fw/styles/nu-fw-fsm.css" rel="stylesheet" type="text/css"/><link href="https://fonts.googleapis.com/css?family=Open+Sans:300,700" rel="stylesheet" type="text/css"/><link href="//deptcommon.fsm.northwestern.edu/_fw/scripts/slick/slick-theme.css" rel="stylesheet" type="text/css"/><link href="//deptcommon.fsm.northwestern.edu/_fw/styles/new-header.css" rel="stylesheet" type="text/css"/><link href="//deptcommon.fsm.northwestern.edu/_fw/styles/announcements.css" rel="stylesheet" type="text/css"/><link href="//deptcommon.fsm.northwestern.edu/_fw/styles/mobile-exp.css" rel="stylesheet" type="text/css"/><!-- END CSS_EXTRA -->
+          <!-- BEGIN CSS_LOCAL --><link href="styles/v2.0.css" rel="stylesheet" type="text/css"/><link href="" rel="stylesheet" type="text/css"/><!-- END CSS_LOCAL -->
+          <!--[if lt IE 9]>
+          <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+          <![endif]-->
+          
+          <!--[if lt IE 9]>
+          <link href="//deptcommon.fsm.northwestern.edu/_fw/respond-proxy" id="respond-proxy" rel="respond-proxy"/>    
+          
+          <link href="//psychiatry-dev.fsm.northwestern.edu/_fw/respond.proxy.gif" id="respond-redirect" rel="respond-redirect"/>
+          <script src="//psychiatry-dev.fsm.northwestern.edu/_fw/respond.proxy.js"></script>
+          <![endif]-->
+          
+          <link rel="canonical" href="https://psychiatry-dev.fsm.northwestern.edu/"/>
+          <!-- START Favicon -->
+          <link rel="apple-touch-icon" sizes="57x57" href="//www.feinberg.northwestern.edu/favicons/apple-icon-57x57.png"/> 
+          <link rel="apple-touch-icon" sizes="60x60" href="//www.feinberg.northwestern.edu/favicons/apple-icon-60x60.png"/> 
+          <link rel="apple-touch-icon" sizes="72x72" href="//www.feinberg.northwestern.edu/favicons/apple-icon-72x72.png"/> 
+          <link rel="apple-touch-icon" sizes="76x76" href="//www.feinberg.northwestern.edu/favicons/apple-icon-76x76.png"/> 
+          <link rel="apple-touch-icon" sizes="114x114" href="//www.feinberg.northwestern.edu/favicons/apple-icon-114x114.png"/> 
+          <link rel="apple-touch-icon" sizes="120x120" href="//www.feinberg.northwestern.edu/favicons/apple-icon-120x120.png"/> 
+          <link rel="apple-touch-icon" sizes="144x144" href="//www.feinberg.northwestern.edu/favicons/apple-icon-144x144.png"/> 
+          <link rel="apple-touch-icon" sizes="152x152" href="//www.feinberg.northwestern.edu/favicons/apple-icon-152x152.png"/> 
+          <link rel="apple-touch-icon" sizes="180x180" href="//www.feinberg.northwestern.edu/favicons/apple-icon-180x180.png"/> 
+          <link rel="icon" type="image/png" href="//www.feinberg.northwestern.edu/favicons/favicon-32x32.png" sizes="32x32"/> 
+          <link rel="icon" type="image/png" href="//www.feinberg.northwestern.edu/favicons/android-icon-192x192.png" sizes="192x192"/> 
+          <link rel="icon" type="image/png" href="//www.feinberg.northwestern.edu/favicons/favicon-96x96.png" sizes="96x96"/> 
+          <link rel="icon" type="image/png" href="//www.feinberg.northwestern.edu/favicons/favicon-16x16.png" sizes="16x16"/> 
+          <link rel="manifest" href="//www.feinberg.northwestern.edu/favicons/manifest.json"/> 
+          <meta name="msapplication-TileColor" content="#61458c"/> 
+          <meta name="msapplication-TileImage" content="//www.feinberg.northwestern.edu/favicons/ms-icon-144x144.png"/> 
+          <meta name="theme-color" content="#61468b"/>
+          <!-- END Favicon -->    
+          <!-- BEGIN CSS_COMMON --><link href="dist/css/pathology-slider.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+    <?php 
 
-?>
+        include('includes/pathology-sliders.php'); 
 
-
-</body>
-<!-- <script type="text/javascript" src="js/output.js" ></script> -->
-<script type="text/javascript" src="dist/js/output.js" ></script>
+    ?>
+    </body>
+    <!-- BEGIN JS_COMMON --><script src="//code.jquery.com/jquery-1.11.0.min.js" type="text/javascript"></script><script src="//cdn.jsdelivr.net/jquery.slick/1.5.9/slick.min.js" type="text/javascript"></script><script src="//deptcommon.fsm.northwestern.edu/_rwd/scripts/respond.min.js" type="text/javascript"></script><script src="//deptcommon.fsm.northwestern.edu/_fw/scripts/common.original.js" type="text/javascript"></script><script src="//deptcommon.fsm.northwestern.edu/scripts/nu-fw-landing-page-script.js" type="text/javascript"></script><script src="//deptcommon.fsm.northwestern.edu/_fw/scripts/slick-home.js" type="text/javascript"></script><script src="//deptcommon.fsm.northwestern.edu/_fw/scripts/mobile-exp.js" type="text/javascript"></script><!-- END JS_COMMON -->
+    <!-- BEGIN JS_EXTRA --><script src="//deptcommon.fsm.northwestern.edu/scripts/ds-scripts/faculty-spotlight-slider.js" type="text/javascript"></script><!-- END JS_EXTRA -->
+    <!-- BEGIN JS_EXTRA2 --><!-- END JS_EXTRA2 -->
+    <script type="text/javascript" src="dist/js/output.js" ></script>
 </html>
