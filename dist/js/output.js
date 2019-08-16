25231fabe36c699d80f29aed7da0a491d02678b8
Duncan Seif <duncan.seif@northwestern.edu>
2019-08-16 12:38:56 -0500

    Got js working and pulling in json file from Cascade

--- dist/js/output.js
+++ dist/js/output.js
@@ -1,11 +1,206 @@
 "use strict";
 
-// var pmg = document.body.querySelector('g#Layer_1');
-// var svg = document.getElementById('prevMedJewel');
-window.onload = function () {
-  fetch('https://feinberg-dev.fsm.northwestern.edu/it-new/ws/purchasing-api.php').then(function (response) {
-    return response.json();
-  }).then(function (myJson) {
-    console.log(JSON.stringify(myJson));
+var __awaiter = void 0 && (void 0).__awaiter || function (thisArg, _arguments, P, generator) {
+  return new (P || (P = Promise))(function (resolve, reject) {
+    function fulfilled(value) {
+      try {
+        step(generator.next(value));
+      } catch (e) {
+        reject(e);
+      }
+    }
+
+    function rejected(value) {
+      try {
+        step(generator["throw"](value));
+      } catch (e) {
+        reject(e);
+      }
+    }
+
+    function step(result) {
+      result.done ? resolve(result.value) : new P(function (resolve) {
+        resolve(result.value);
+      }).then(fulfilled, rejected);
+    }
+
+    step((generator = generator.apply(thisArg, _arguments || [])).next());
   });
 };
+
+var __generator = void 0 && (void 0).__generator || function (thisArg, body) {
+  var _ = {
+    label: 0,
+    sent: function sent() {
+      if (t[0] & 1) throw t[1];
+      return t[1];
+    },
+    trys: [],
+    ops: []
+  },
+      f,
+      y,
+      t,
+      g;
+  return g = {
+    next: verb(0),
+    "throw": verb(1),
+    "return": verb(2)
+  }, typeof Symbol === "function" && (g[Symbol.iterator] = function () {
+    return this;
+  }), g;
+
+  function verb(n) {
+    return function (v) {
+      return step([n, v]);
+    };
+  }
+
+  function step(op) {
+    if (f) throw new TypeError("Generator is already executing.");
+
+    while (_) {
+      try {
+        if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+        if (y = 0, t) op = [op[0] & 2, t.value];
+
+        switch (op[0]) {
+          case 0:
+          case 1:
+            t = op;
+            break;
+
+          case 4:
+            _.label++;
+            return {
+              value: op[1],
+              done: false
+            };
+
+          case 5:
+            _.label++;
+            y = op[1];
+            op = [0];
+            continue;
+
+          case 7:
+            op = _.ops.pop();
+
+            _.trys.pop();
+
+            continue;
+
+          default:
+            if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) {
+              _ = 0;
+              continue;
+            }
+
+            if (op[0] === 3 && (!t || op[1] > t[0] && op[1] < t[3])) {
+              _.label = op[1];
+              break;
+            }
+
+            if (op[0] === 6 && _.label < t[1]) {
+              _.label = t[1];
+              t = op;
+              break;
+            }
+
+            if (t && _.label < t[2]) {
+              _.label = t[2];
+
+              _.ops.push(op);
+
+              break;
+            }
+
+            if (t[2]) _.ops.pop();
+
+            _.trys.pop();
+
+            continue;
+        }
+
+        op = body.call(thisArg, _);
+      } catch (e) {
+        op = [6, e];
+        y = 0;
+      } finally {
+        f = t = 0;
+      }
+    }
+
+    if (op[0] & 5) throw op[1];
+    return {
+      value: op[0] ? op[1] : void 0,
+      done: true
+    };
+  }
+};
+
+var Store =
+/** @class */
+function () {
+  function Store(containerID) {
+    this.apiURL = 'https://feinberg-dev.fsm.northwestern.edu/it-new/ws/purchasing-api.php';
+    this.items = [{}];
+    this.containerEL = document.getElementById(containerID);
+    this.stockTheShelves();
+  }
+
+  Store.prototype.loadProducts = function () {
+    var _this = this;
+
+    return fetch(this.apiURL).then(function (response) {
+      //if you dont do another then, code executes before promise returns
+      return response.json();
+    }).then(function (myJson) {
+      var result = myJson.items;
+
+      if (result.length > 0) {
+        _this.items = myJson.items;
+        return true;
+      } else {
+        return false;
+      }
+    });
+  };
+
+  Store.prototype.stockTheShelves = function () {
+    return __awaiter(this, void 0, void 0, function () {
+      var result, shelves_1;
+      return __generator(this, function (_a) {
+        switch (_a.label) {
+          case 0:
+            return [4
+            /*yield*/
+            , this.loadProducts()];
+
+          case 1:
+            result = _a.sent();
+
+            if (result) {
+              shelves_1 = "<div class=\"block-wrapper\"><section class=\"shelves\"><div class=\"feature-three-col modBreakFour\">";
+              this.items.forEach(function (item) {
+                shelves_1 += "<article class=\"feature-box\">\n                                    <center>\n                                        <img src=\"assets/png/300x200.png\" />\n                                    </center>\n                                    <div class=\"feature-copy\">\n                                        <h6>" + item.title + "</h6>\n                                    </div>\n                                    <a class=\"button\" href=\"publications/index.html\">Add To Cart</a>\n                                </article>";
+              });
+              shelves_1 += "</div></section></div>";
+              console.log(shelves_1);
+              this.containerEL.insertAdjacentHTML('beforeend', shelves_1);
+            }
+
+            return [2
+            /*return*/
+            ];
+        }
+      });
+    });
+  };
+
+  return Store;
+}();
+
+window.onload = function () {
+  var store = new Store('main-content');
+}; // <section class="contain-1120"><div class="feature-three-col width-1120 policies"><article class="feature-box"><a href="information-security/index.html" title="Information Security"><div class="feature-copy"><div class="iBox"><i class="fas fa-shield-alt"></i></div><h4>Information Security</h4><p>Get details on information protection required by Feinberg</p></div></a></article>
--- js/itCart.ts
+++ js/itCart.ts
@@ -1,14 +1,80 @@
 
 // var pmg = document.body.querySelector('g#Layer_1');
 // var svg = document.getElementById('prevMedJewel');
+interface item {
+    id: number;
+    title: string;
+    element: [];
+    type: string;
+    categories: [];
+    price: number;
+    desc: string;
+}
+
+
+class Store {
+    private apiURL = 'https://feinberg-dev.fsm.northwestern.edu/it-new/ws/purchasing-api.php';
+    private containerEL:HTMLElement;
+    private items = [<item>{}];
+    private sortBy: string;
+
+    constructor( containerID ) {
+        this.containerEL = document.getElementById( containerID );
+        this.stockTheShelves();
+    }
+
+    loadProducts() {
+        return fetch( this.apiURL ).then( (response) => {
+            //if you dont do another then, code executes before promise returns
+            return response.json();
+        }).then( (myJson) => {
+            
+            let result = myJson.items;
+
+            if(result.length > 0){
+                this.items = myJson.items;
+                return true;
+            } else {
+                return false;
+            }
+        });
+    }
+
+   async stockTheShelves(){
+
+        let result = await this.loadProducts();
+
+        if( result ){
+
+            let shelves = `<div class="block-wrapper"><section class="shelves"><div class="feature-three-col modBreakFour">`;
+
+                this.items.forEach( ( item ) => {
+                    shelves += `<article class="feature-box">
+                                    <center>
+                                        <img src="assets/png/300x200.png" />
+                                    </center>
+                                    <div class="feature-copy">
+                                        <h6>${item.title}</h6>
+                                    </div>
+                                    <a class="button" href="publications/index.html">Add To Cart</a>
+                                </article>`;
+                });
+
+            shelves += `</div></section></div>`;
+
+            console.log(shelves);
+
+            this.containerEL.insertAdjacentHTML('beforeend', shelves);
+        }
+    }
+
+
+}
+
 
 window.onload=function() {
-
-    fetch('https://feinberg-dev.fsm.northwestern.edu/it-new/ws/purchasing-api.php').then(function(response) {
-        return response.json();
-    }).then(function(myJson) {
-        console.log(JSON.stringify(myJson));
-    });
-
+    let store = new Store('main-content');
 };
 
+
+// <section class="contain-1120"><div class="feature-three-col width-1120 policies"><article class="feature-box"><a href="information-security/index.html" title="Information Security"><div class="feature-copy"><div class="iBox"><i class="fas fa-shield-alt"></i></div><h4>Information Security</h4><p>Get details on information protection required by Feinberg</p></div></a></article>
